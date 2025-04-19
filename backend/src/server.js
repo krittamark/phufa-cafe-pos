f6cfbf1ae78cae92df82b25fbc52d8f4caae34e1
krittamark <33484828+krittamark@users.noreply.github.com>
2025-04-20 02:25:25 +0700

    feat: Configure environment variables and add database connection setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({path: '.env'});
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
