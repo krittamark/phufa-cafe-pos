9d9a8be3888063e2b0e7f4cf8cfe053922a6379d
Krittamet Damthongkam <33484828+krittamark@users.noreply.github.com>
2025-04-20 14:40:25 +0700

    Delete backend/src/controllers/Order directory

--- backend/src/controllers/Order/CreateNewOrder.controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = (request, response, next) => {
-  return response.status(200).json({
-    status: "ok",
-    message: "CreateNew is operational.",
-    serviceName: "PhufaCafeAPI",
-    timestamp: new Date().toISOString(),
-  });
-};
