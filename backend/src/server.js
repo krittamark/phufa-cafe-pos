bd256f91113d25a5065e31999c32df97ebb7d258
krittamark <33484828+krittamark@users.noreply.github.com>
2025-04-20 14:47:35 +0700

    fix: update import path for OrderRoutes in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const OrderRoutes = require("./routes/Menu.route");
+const OrderRoutes = require("./routes/order.route");
 
 app.use(express.json());
 
