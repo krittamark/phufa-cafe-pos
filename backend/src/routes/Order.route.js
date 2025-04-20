7b124d51992844cc97a9d04961d100954977b606
Krittamet Damthongkam <33484828+krittamark@users.noreply.github.com>
2025-04-20 14:39:59 +0700

    Delete backend/src/routes/Order.route.js

--- backend/src/routes/Order.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const CreateNewOrder = require('../controllers/Order/CreateNewOrder.controller')
-
-router.post('/', CreateNewOrder)
-
-module.exports = router
